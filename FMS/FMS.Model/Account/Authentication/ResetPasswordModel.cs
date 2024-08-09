74da524ef7a90e4d2159994669536b4310cd25b5
Pintu Ray <100366928+PintuRay@users.noreply.github.com>
2024-08-09 07:05:14 +0530

    chanes

--- FMS/FMS.Model/Account/Authentication/ResetPasswordModel.cs
+++ FMS/FMS.Model/Account/Authentication/ResetPasswordModel.cs
@@ -5,6 +5,7 @@ namespace FMS.Model.Account.Authentication
     public class ResetPasswordModel
     {
         public string NewPassword { get; set; }
+        [Compare("NewPassword")]
         public string ConfirmNewPassword { get; set; }
     }
 }
