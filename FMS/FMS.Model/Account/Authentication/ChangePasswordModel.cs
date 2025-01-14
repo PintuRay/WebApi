0bb10deac50b10e74e1eb91212361684bf4453af
Pintu Ray <100366928+PintuRay@users.noreply.github.com>
2025-01-14 15:42:08 +0530

    changes

--- FMS/FMS.Model/Account/Authentication/ChangePasswordModel.cs
+++ FMS/FMS.Model/Account/Authentication/ChangePasswordModel.cs
@@ -1,7 +1,12 @@
-﻿namespace FMS.Model.Account.Authentication
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FMS.Model.Account.Authentication
 {
-    public class ChangePasswordModel : ResetPasswordModel
+    public class ChangePasswordModel 
     {
         public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        [Compare("NewPassword")]
+        public string ConfirmNewPassword { get; set; }
     }
 }
